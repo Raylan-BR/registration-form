d1b43829655e1fb202bb71389f3c1814e756360d
Raylan <raylanbruno2002@gmail.com>
2025-03-30 14:33:52 -0300

    validação do cadastro

--- frontend/js/cadastro.js
+++ frontend/js/cadastro.js
@@ -1,5 +1,28 @@
 // implementar botão de ir para login
-
 function ir_tela_login(){
-    window.location.href = "../public/login.html";
+    //captura os dados dos inputs do cadastro
+    var nome_usuario = document.getElementById("nome_usuario").value;
+    var senha = document.getElementById("senha").value;
+    
+    //validar o acesso a tela de login
+    if(nome_usuario != "" && senha != ""){
+
+        if(senha.length > 8){
+            alert("Ops, sua senha deve ter apenas 8 caracteres!");
+        }
+        else if(nome_usuario.length > 15){
+            alert("Opa, seu nome de usuário é muito longo!");
+        }
+        else{
+            localStorage.setItem(nome_usuario, senha);
+            console.log(nome_usuario," salvo no localStorage");
+
+            window.location.href = "../public/login.html";
+        }
+    }
+    else {
+        console.log("cadastro concluído!");
+        validar_cadastro_aviso = document.getElementById("validar_campos_cadastro");
+        validar_cadastro_aviso.style.opacity = 1;
+    }
 }
