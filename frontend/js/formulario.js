913180eb2c604810af3641656036df6ae2a0d18a
Raylan <raylanbruno2002@gmail.com>
2025-03-31 19:11:55 -0300

    validação das informações de endereço

--- frontend/js/formulario.js
+++ frontend/js/formulario.js
@@ -42,13 +42,62 @@ document.querySelector("#entrada_telefone").addEventListener("input",function(ev
     event.target.value = telefone;
 });
 //validação ao anexar a identidade
-document.querySelector("#anexar_identidade").addEventListener("change", function(event){
-    let identidade = event.target.files[0];
-    let confirmar_anexo_texto = document.querySelector("#confirmar_anexo_texto");
-    
-    if(identidade){
-        confirmar_anexo_texto.style.width = "300px";
-        confirmar_anexo_texto.innerHTML = identidade.name;
-    }
-});
 
+/*Nessa função é possivel controlar o fluxo de funcionamento 
+dos inputs files atraves do id e do parágrafo para confirmação*/
+function validar_anexo(identificador, texto_visual){
+    document.querySelector(`#${identificador}`).addEventListener("change", function(event){
+        let identidade = event.target.files[0];
+        let confirmar_anexo_texto = document.querySelector(`#${texto_visual}`);
+        
+        if(identidade){
+            confirmar_anexo_texto.style.width = "300px";
+            confirmar_anexo_texto.innerHTML = identidade.name;
+        }
+    });
+}
+validar_anexo("anexar_identidade", "confirmar_anexo_texto_identidade");
+
+//validar CEP e preencher automaticamente
+/*aqui podemos usar uma api simples que vai 
+fornecer os dados de endereço a partir do cep*/
+
+document.querySelector("#entrada_cep").addEventListener("input", buscarEndereco);
+
+function buscarEndereco(event){
+    let cep = event.target.value;
+    //restringe letras no input
+    cep = cep.replace(/\D/g,"");
+
+    if(cep.length < 8){
+        document.querySelector("#validar_endereco").style.opacity = 1;
+        //os inputs abaixo devem ficar vazios;
+        document.querySelector("#entrada_rua").value = "";
+        document.querySelector("#entrada_cidade").value = "";
+        document.querySelector("#entrada_estado").value = "";
+        return;
+    }
+    event.target.value = cep.replace(/(\d{5})(\d{3})/,"$1-$2");
+    //caso o CEP esteja certo, o aviso deve ficar invisível
+    document.querySelector("#validar_endereco").style.opacity =  0;
+
+    let url = `https://viacep.com.br/ws/${cep}/json/`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            if(data.erro){
+                alert("CEP não encontrado!");
+            }
+            else{
+                document.querySelector("#entrada_rua").value = data.logradouro;
+                document.querySelector("#entrada_cidade").value = data.localidade;
+                document.querySelector("#entrada_estado").value = data.uf;
+            }
+        })
+        .catch(error => console.error("Erro na requisição:", error));
+
+}
+//validação ao anexar o comprovante de residencia
+
+validar_anexo("anexar_comprovante", "confirmar_anexo_texto_endereco");
